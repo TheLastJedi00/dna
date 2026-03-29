4477e70eab51f1d0dc1450ed5554bb3806f31ed3
Jediael Borges <jediaelborges23@gmail.com>
2026-03-29 13:04:14 -0300

    feat: footer completo

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { ElevatedButton } from '../../shared/buttons/elevated-button/elevated-button';
 import { BgImageCard } from "../../shared/cards/bg-image-card/bg-image-card";
 import { LandingHeader } from "../../shared/headers/landing-header/landing-header"; 
+import { LandingFooter } from "../../shared/footers/landing-footer/landing-footer";
 
 @Component({
   selector: 'app-landing',
-  imports: [ElevatedButton, BgImageCard, LandingHeader],
+  imports: [ElevatedButton, BgImageCard, LandingHeader, LandingFooter],
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
 })
