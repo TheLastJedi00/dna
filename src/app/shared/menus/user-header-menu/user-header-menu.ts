09106bfe74a6d868ee4258d78a0c86b71127bf5b
Jediael Borges <jediaelborges23@gmail.com>
2026-03-31 08:06:27 -0300

    feat: botão open/close do menu de user dashboard header está pronto

--- src/app/shared/menus/user-header-menu/user-header-menu.ts
+++ src/app/shared/menus/user-header-menu/user-header-menu.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { NgClass } from "@angular/common";
 
 @Component({
   selector: 'app-user-header-menu',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './user-header-menu.html',
   styleUrl: './user-header-menu.scss',
 })
 export class UserHeaderMenu {
   hrItems = Array(3).fill(0);
+  toggle = false;
+
+  toggleMenu() {
+    this.toggle = !this.toggle;
+  }
 }
