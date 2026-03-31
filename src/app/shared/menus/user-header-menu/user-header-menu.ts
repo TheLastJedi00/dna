b3423e646dc06e58846860a206e5ca780c165249
Jediael Borges <jediaelborges23@gmail.com>
2026-03-31 09:43:10 -0300

    feat: animações de expansão do menu lateral

--- src/app/shared/menus/user-header-menu/user-header-menu.ts
+++ src/app/shared/menus/user-header-menu/user-header-menu.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NgClass } from "@angular/common";
+import { TextButton } from "../../buttons/text-button/text-button";
 
 @Component({
   selector: 'app-user-header-menu',
-  imports: [NgClass],
+  imports: [NgClass, TextButton],
   templateUrl: './user-header-menu.html',
   styleUrl: './user-header-menu.scss',
 })
