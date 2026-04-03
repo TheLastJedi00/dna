f9720d0d2cbc2240c2c31833826ebc99569e1ee3
Jediael Borges <jediaelborges23@gmail.com>
2026-04-03 19:14:23 -0300

    feat: logo no  header serve como home button

--- src/app/shared/headers/user-panel-header/user-panel-header.ts
+++ src/app/shared/headers/user-panel-header/user-panel-header.ts
@@ -1,6 +1,7 @@
-import { Component, output } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
 import { Logo } from "../../logo/logo";
 import { UserHeaderMenu } from '../../menus/user-header-menu/user-header-menu';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-panel-header',
@@ -9,9 +10,14 @@ import { UserHeaderMenu } from '../../menus/user-header-menu/user-header-menu';
   styleUrl: './user-panel-header.scss',
 })
 export class UserPanelHeader {
+  router = inject(Router);
   logout = output<void>();
 
   onLogout() {
     this.logout.emit();
+  }
+
+  returnToHome(){
+    this.router.navigate(['user-dashboard'])
   }
 }
