d7e7f1366c9305c31df58b4764078ed4314bed40
TheLastJedi00 <jediaelborges23@gmail.com>
2026-05-15 16:13:05 -0300

    fix: api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    apiUrl: 'https://console.cloud.google.com/cloud-build/builds;region=global/9cf6a18d-75f8-461d-874c-2f5bbb82c3cb?project=dna-empresaria-6c142',
+    apiUrl: 'https://dna-api-773557020109.europe-west1.run.app',
 };
